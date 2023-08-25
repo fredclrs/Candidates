78e755ea63df1ada4de17974533bba8eb0bd91bf
fredclrs <73029347+fredclrs@users.noreply.github.com>
2023-08-25 18:22:42 -0400

    Add files via upload

---
+++ CandidateController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using TestCandidates.Models;
+using TestCandidates.Models.Daos;
+
+namespace TestCandidates.Controllers
+{
+    public class CandidateController : Controller
+    {        
+        private CandidateDao _candidateDao;
+
+        public CandidateController()
+        {
+            _candidateDao = new CandidateDao();
+        }
+
+        // GET: Candidate
+        public IActionResult Index()
+        {
+            return View(_candidateDao.getAll());
+        }
+
+        // GET: Candidate/Details/5
+        /*public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _candidateDao.getAll() == null)
+            {
+                return NotFound();
+            }
+
+            var candidate = await _context.Candidates
+                .FirstOrDefaultAsync(m => m.IdCandidate == id);
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidate);
+        }*/
+
+        // GET: Candidate/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidate/Create        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Candidate candidate)
+        {
+            candidate.InsertDate = DateTime.Now.Date;
+           _candidateDao.addCandidate(candidate);                
+           return RedirectToAction(nameof(Index));
+                        
+        }
+
+        // GET: Candidate/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            var candidate = _candidateDao.getCandidate(Convert.ToInt32(id));
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+            return View(candidate);
+        }
+
+        // POST: Candidate/Edit/5        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Candidate candidate)
+        {
+            try
+            {
+                var vcandidate = _candidateDao.getCandidate(candidate.IdCandidate);
+                candidate.InsertDate = vcandidate.InsertDate;
+                candidate.ModifyDate = DateTime.Now.Date;
+                _candidateDao.editCandidate(candidate);
+                return RedirectToAction("Index");
+            }
+            catch {
+                return View();
+            }            
+        }
+
+        // GET: Candidate/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidate = _candidateDao.getCandidate(Convert.ToInt32(id));
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidate);
+        }
+
+        // POST: Candidate/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Candidate candidate)
+        {
+            try
+            {
+                _candidateDao.deleteCandidate(candidate);
+                return RedirectToAction("Index");
+            }
+            catch {
+                return View();
+            }
+        }
+        
+    }
+}
---
+++ CandidateExperiencieController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCandidates.Models;
+using TestCandidates.Models.Daos;
+
+namespace TestCandidates.Controllers
+{
+    public class CandidateExperiencieController : Controller
+    {
+        private  CandidateExperiencieDao _candidateExDao;
+        private CandidateDao _candidateDao;
+        private static int _idCandidate;
+
+        public CandidateExperiencieController()
+        {            
+            _candidateDao = new CandidateDao();
+            _candidateExDao = new CandidateExperiencieDao();
+        }
+
+        // GET: CandidateExperiencie
+        public IActionResult Index(int? id)
+        {
+            if (id != null) {
+                _idCandidate = Convert.ToInt32(id);
+            }                        
+            return View(_candidateExDao.getAll(Convert.ToInt32(_idCandidate)));
+        }
+        
+        // GET: CandidateExperiencie/Create
+        public IActionResult Create()
+        {           
+            return View();
+        }
+
+        // POST: CandidateExperiencie/Create        
+        [HttpPost, ActionName("Create")]       
+        public IActionResult Create(CandidateExperiencie candidateExperiencie)
+        {
+            try
+            {
+                candidateExperiencie.InsertDate = DateTime.Now.Date;
+                candidateExperiencie.IdCandidate = _idCandidate;
+                _candidateExDao.addExperiencie(candidateExperiencie);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex) {
+                return View(ex.Message);
+            }
+            
+        }
+
+        // GET: CandidateExperiencie/Edit/5
+        public IActionResult Edit(int? id)
+        {
+           if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidateExperiencie = _candidateExDao.GetCandidateExperiencie(Convert.ToInt32(id));
+            if (candidateExperiencie == null)
+            {
+                return NotFound();
+            }            
+            return View(candidateExperiencie);
+        }
+
+        // POST: CandidateExperiencie/Edit/5        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CandidateExperiencie candidateExperiencie)
+        {          
+             try
+             {
+                var candidateExp = _candidateExDao.GetCandidateExperiencie(candidateExperiencie.IdCandidateExperiencie);
+                candidateExperiencie.InsertDate = candidateExp.InsertDate;
+                candidateExperiencie.IdCandidate = candidateExp.IdCandidate;
+                candidateExperiencie.ModifyDate = DateTime.Now.Date;
+                _candidateExDao.editExperiencie(candidateExperiencie);
+                return RedirectToAction("Index");
+             }
+             catch 
+             {
+                return View();
+             }                                        
+        }
+
+        // GET: CandidateExperiencie/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidateExperiencie = _candidateExDao.GetCandidateExperiencie(Convert.ToInt32(id));
+            if (candidateExperiencie == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidateExperiencie);
+        }
+
+        // POST: CandidateExperiencie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(CandidateExperiencie candidateExperiencie)
+        {
+
+            _candidateExDao.deleteExperiencie(candidateExperiencie);
+            return RedirectToAction(nameof(Index));
+        }        
+    }
+}
